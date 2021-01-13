ac24b9eaa435f3ab933e5ababdb4ae6639a80ed2
Bjorn Yourey <byourey7783@gmail.com>
2021-01-13 12:59:46 -0500

    created function to start questions

--- script.js
+++ script.js
@@ -1,21 +1,64 @@
-// // // Added event listener to start quiz button
 
-
-// const option1 = document.querySelector("option1");
-// const option2 = document.querySelector("option2");
-// const option3 = document.querySelector("option3");
-// const option4 = document.querySelector("option4");
-
-// const next_question = document.querySelector(".next_button");
-
-
-// start.addEventListener("click", ()=>{
-//     start.style.display = "none";
-//     rules.style.display = "block";
-// })
+let allQuestions = [
+    {
+        question:'What does CSS stand for?',
+        option: [
+        'Common Solution Sheet',
+        'Cascading Style Sheet',
+        'Cascading Solution Style',
+        'Computer Style Sheet',
+           ],
+        answer: 2,
+    },
+    {
+        question: 'Which HTML tag is used to define an internal style sheet?',
+        option: [
+        '<script>',
+        '<style>',
+        '<html>',
+        '<body>',
+    ],
+        answer: 2,
+    },
+    {
+        question: 'Which is the correct CSS syntax?',
+        option: [
+        'body{color:blue}',
+        '{body{color:blue}',
+        'body={color:blue}',
+        'body:color:blue}',
+        ],
+        answer: 2,
+    },
+    {
+        question: 'What is 22 + 2?',
+        option:[
+        '2',
+        '3',
+        '4',
+        '10',
+        ],
+        answer: 2,
+    },
+    {
+        question: 'What is 222 + 2?',
+        option:[
+        '2',
+        '3',
+        '4',
+        '10',
+        ],
+        answer: 2,
+    }
+]
 
 const questionContainerElement = document.getElementById('question-container')
 const questionBoxElement = document.querySelector('#questionBox')
+let index = 0;
+let quest = allQuestions.sort(function(){
+return 0.5 - Math.random();
+})
+
 
 // Added a timer for the quiz
 const timeLeftDisplay = document.querySelector('#time-left')
@@ -36,8 +79,10 @@ function timeout() {
 }
 startBtn.addEventListener("click", timeout)
 
+// Added event listener for the start quiz button
 startBtn.addEventListener("click", startQuiz)
 
+// Added a function for what should happen after clicking the start button
 function startQuiz() {
     startBtn.classList.add("hide")
     timeLeftDisplay.classList.remove("hide")
@@ -48,83 +93,15 @@ function startQuiz() {
 
 // nextBtn.addEventListener("click", function(){
 
+startQuestions(index);
 
-
-
-
-// var document.getElementById("questions", )
-
-
-// const startButton = document.getElementById("btn-start");
-// const questionsEl = document.querySelector(".ques");
-// const quizBox = document.querySelector(".box");
-// // startButton.addEventListener("click", startQuiz)
-
-// function to start the quiz, function to bring in the questions.
-// function startQuiz() {
-// console.log('started')
-// // startButton.classList.add('hide')
-// // questionsEl.classList.remove('hide')
-// // setNextQues()
-// }
-
-
-
-// // function to reduce time when a question is answered incorrectly.
-// // create a function to display if answer is right
-// // create a function to add points 
-// 
-
-// to end the Game
-// to enter initials (another div to create and is hidden till gave is over)
-// display the Score (another div to create and is hidden till gave is over) put them below questions div
-
-// some document,getElementById maybe globalT 
-// use regular functions
-
-// let allQuestions = [
-//     {
-//         question:'What does HTML stand for?',
-//         choice1:'Hypher Text Making Language',
-//         choice2:'Hyper Text Markup Language',
-//         choice3:'Hyper Text Making Language',
-//         choice4:'Hyper Text Mark Language',
-//         answer: 2,
-//     },
-//     {
-//         question: 'Which HTML tag is used to define an internal style sheet?',
-//         choice1:'<script>',
-//         choice2:'<style>',
-//         choice3:'<html>',
-//         choice4:'<body>',
-//         answer: 2,
-//     },
-//     {
-//         question: 'Which is the correct CSS syntax?',
-//         choice1:'body{color:blue}',
-//         choice2:'{body{color:blue}',
-//         choice3:'body={color:blue}',
-//         choice4:'body:color:blue}',
-//         answer: 2,
-//     },
-// //     {
-// //         question: 'What is 22 + 2?',
-// //         choice1:'2',
-// //         choice2:'3',
-// //         choice3:'4',
-// //         choice4:'10',
-// //         answer: 2,
-// //     },
-// //     {
-// //         question: 'What is 222 + 2?',
-// //         choice1:'2',
-// //         choice2:'3',
-// //         choice3:'4',
-// //         choice4:'10',
-// //         answer: 2,
-// //     }
-// // ]
-
-
+// Created a function to start the questions
+function startQuestions(i) {
+     $(".questionBox").text(quest[i].question);
+     $(".optionBox btn").eq(0).text(quest[i].option[0]);
+     $(".optionBox btn").eq(1).text(quest[i].option[1]);
+     $(".optionBox btn").eq(2).text(quest[i].option[2]);
+     $(".optionBox btn").eq(3).text(quest[i].option[3]);
+}
 
 
